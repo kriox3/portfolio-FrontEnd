8c6047c9b944b3d7bd7a38838bb3a1d526fdd4e3
kriox3 <kkrriiooxx33@gmail.com>
2022-11-15 09:50:34 -0300

    cambioQuincena

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   secret: "campanitas",
   tk: "X%GH~4L%1bdo",
   offline: "http://localhost:8080",
-  servidor: "https://portfolioback-production.up.railway.app",
-  servidor2: "https://portfolioback-production.up.railway.app"
+  servidor2: "https://portfolioback-production.up.railway.app",
+  servidor:  "https://portfolioback2-production.up.railway.app"
 };
